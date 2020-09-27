398773af6ff85074ba7ee8f006217f5f31ff7683
lin charlie <a755405@stdmail.nssh.ntpc.edu.tw>
2020-09-27 14:50:41 +0800

    homework

--- Multiple.cpp
+++ Multiple.cpp
@@ -1,10 +1,10 @@
-﻿#include <iostream>
+#include <iostream>
 using namespace std;  
 int main() {
   int num;
   cout << "請輸入一個數字 :\n";
   cin >> num;
-  if (num % 3 ==0){
+  if (num % 3 == 0){
     cout << "這是3的倍數";
   } else {
     cout << "這不是3的倍數";
